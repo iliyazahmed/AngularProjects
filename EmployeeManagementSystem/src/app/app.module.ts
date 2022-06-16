c0d848973be43ce785c866d9555216d1ca7ecc65
Mohammad Iliyaz <91978876+iliyazahmed@users.noreply.github.com>
2022-06-16 16:00:58 +0530

    Angular First initial Commit

---
+++ EmployeeManagementSystem/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeDashBoardComponent } from './MyComponents/employee-dash-board/employee-dash-board.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeDashBoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
