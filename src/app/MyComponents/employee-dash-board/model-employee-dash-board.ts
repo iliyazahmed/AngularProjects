364fdba74cf392bde7b888b98d7b65df9079597b
iliyaz <iliyaz@thinknsolutions.com>
2022-06-16 15:43:49 +0530

    update

---
+++ src/app/MyComponents/employee-dash-board/model-employee-dash-board.ts
@@ -0,0 +1,8 @@
+export class EmployeeDetails {
+    empId: number=0;
+    empName: string = '';
+    emailId: string = '';
+    mobileNo: number = 0;
+    empDesignation :string ='';
+    salary : number = 0;
+}
