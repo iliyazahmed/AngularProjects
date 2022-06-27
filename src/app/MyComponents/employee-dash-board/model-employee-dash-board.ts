5fe6549f6ab1d9e71e15981f449ba434b089a196
iliyaz <iliyaz@thinknsolutions.com>
2022-06-27 19:33:04 +0530

    added fun

--- src/app/MyComponents/employee-dash-board/model-employee-dash-board.ts
+++
@@ -1,8 +0,0 @@
-export class EmployeeDetails {
-    empId: number=0;
-    empName: string = '';
-    emailId: string = '';
-    mobileNo: number = 0;
-    empDesignation :string ='';
-    salary : number = 0;
-}
