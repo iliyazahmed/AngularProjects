9574c0b3eda49038d720e2dc732161e91acac4ad
iliyaz <iliyaz@thinknsolutions.com>
2022-06-27 19:19:13 +0530

    added functionalites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
+import { AcceptCookiesComponent } from './feature/accept-cookies/accept-cookies.component';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Ng2OrderModule } from 'ng2-order-pipe';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeDashBoardComponent } from './MyComponents/employee-dash-board/employee-dash-board.component';
+import { EmployeeDashBoardComponent } from './core/employee-dash-board/employee-dash-board.component';
+import { SearchComponent } from './feature/search/search.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeDashBoardComponent
+    EmployeeDashBoardComponent,
+    SearchComponent,
+    AcceptCookiesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    Ng2SearchPipeModule,
+    Ng2OrderModule,
+    NgxPaginationModule,
+    CommonModule,
+    FormsModule,
+    NgSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  // tittle : "This is for production enviroment",
+  // apiURL :"http://localhost:3000/posts"
 };
